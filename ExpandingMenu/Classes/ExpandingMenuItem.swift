94756301e275b533c52dee5d317b54bbd6a78465
Usman Mukhtar <usmanmukhtar432@gmail.com>
2020-03-20 18:10:21 +0500

    WIP

--- ExpandingMenu/Classes/ExpandingMenuItem.swift
+++ ExpandingMenu/Classes/ExpandingMenuItem.swift
@@ -29,7 +29,7 @@ open class ExpandingMenuItem: UIView {
         }
     }
     
-    @objc open var titleMargin: CGFloat = 5.0
+    @objc open var titleMargin: CGFloat = 10.0
     
     #if swift(>=4.2)
     @objc open var titleColor: UIColor? {
