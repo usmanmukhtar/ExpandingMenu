e9cb7928b99772b784ee533ef8bd1f7cad240906
Usman Mukhtar <usmanmukhtar432@gmail.com>
2020-03-20 17:10:05 +0500

    bring title close to icon

--- ExpandingMenu/Classes/ExpandingMenuButton.swift
+++ ExpandingMenu/Classes/ExpandingMenuButton.swift
@@ -522,7 +522,7 @@ open class ExpandingMenuButton: UIView, UIGestureRecognizerDelegate {
                 
                 switch self.menuTitleDirection {
                 case .left:
-                    originX = endPoint.x + item.bounds.width / 2.0 + titleButton.bounds.width
+                    originX = endPoint.x + margin - titleButton.bounds.width
                 case .right:
                     originX = endPoint.x + item.bounds.width / 2.0 + margin;
                 }
